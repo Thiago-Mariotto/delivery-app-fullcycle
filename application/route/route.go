0ce3de8b27b2e4562d99c877b61e0be18082fe37
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-06-22 14:50:08 -0300

    feat: add loadPosition funciton

---
+++ application/route/route.go
@@ -0,0 +1,54 @@
+package route
+
+import (
+	"bufio"
+	"errors"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Position struct {
+	Lat  float64
+	Long float64
+}
+
+type Route struct {
+	Id       string
+	ClientId string
+	Position []Position
+}
+
+func (route *Route) LoadPositions() error {
+	if route.Id == "" {
+		return errors.New("route id not informed")
+	}
+
+	file, err := os.Open("destination/" + route.Id + ".txt")
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		data := strings.Split(scanner.Text(), ",")
+		lat, err := strconv.ParseFloat(data[0], 64)
+		if err != nil {
+			return nil
+		}
+
+		long, err := strconv.ParseFloat(data[1], 64)
+		if err != nil {
+			return nil
+		}
+
+		route.Position = append(route.Position, Position{
+			Lat: lat, Long: long,
+		})
+	}
+
+	return nil
+}
